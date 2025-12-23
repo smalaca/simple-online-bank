14956d88e990c6859427ebbda2eb14d69370f14b
smalaca <sebastian.malaca@gmail.com>
2025-12-23 14:14:51 +0100

    add @Repository annotation to repository interfaces

--- src/main/java/com/smalaca/onlinebank/repository/AccountRepository.java
+++ src/main/java/com/smalaca/onlinebank/repository/AccountRepository.java
@@ -3,10 +3,12 @@ package com.smalaca.onlinebank.repository;
 import com.smalaca.onlinebank.domain.Account;
 import com.smalaca.onlinebank.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
     Optional<Account> findByAccountNumber(String accountNumber);
     List<Account> findByCustomer(Customer customer);
--- src/main/java/com/smalaca/onlinebank/repository/CustomerRepository.java
+++ src/main/java/com/smalaca/onlinebank/repository/CustomerRepository.java
@@ -2,9 +2,11 @@ package com.smalaca.onlinebank.repository;
 
 import com.smalaca.onlinebank.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Optional<Customer> findByCustomerNumber(String customerNumber);
 }
